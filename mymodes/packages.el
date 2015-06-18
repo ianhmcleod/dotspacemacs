26814fee57aa4b9266f95361f1c7c70ee00e667e
ianhmcleod <ianhmcleod@gmail.com>
2015-06-18 17:19:19 -0500

    init commit

---
+++ mymodes/packages.el
@@ -0,0 +1,39 @@
+;;; packages.el --- mymodes Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(defvar mymodes-packages
+  '(
+    ;; package mymodess go here
+    elixir-mode
+    alchemist
+    ))
+
+;; List of packages to exclude.
+(defvar mymodes-excluded-packages '())
+
+;; For each package, define a function mymodes/init-<package-mymodes>
+;;
+;; (defun mymodes/init-my-package ()
+;;   "Initialize my package"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
+(defun mymodes/init-elixir-mode ()
+  (use-package elixir-mode)
+  )
+(defun mymodes/init-alchemist()
+  (use-package alchemist)
+  )
